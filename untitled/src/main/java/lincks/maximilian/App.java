391e7b34fbd44a822a7eb5e3ad1bc967cd4fa766
Maxi <maximilian.lincks@gmx.net>
2024-05-10 22:48:07 +0200

    initial commit

---
+++ untitled/src/main/java/lincks/maximilian/App.java
@@ -0,0 +1,13 @@
+package lincks.maximilian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
